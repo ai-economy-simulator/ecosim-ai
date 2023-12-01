9fd35797c2e5d976f7d5082a663923bcf5145d34
Amish Mittal <amishmittal@outlook.com>
2023-12-01 23:53:42 +0530

    initialize FastAPI server

---
+++ main.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/newGameCode")
+def new_game_code():
+    return {"gameCode": "123"}
